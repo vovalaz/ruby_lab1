e04cb5c2c30eeb5ef1adbfac6eb5fe4f0d3b83b6
Yakiv <yanyk@bamboo-card.com>
2023-12-16 10:53:30 +0200

    demo file was added

---
+++ demo.rb
@@ -0,0 +1,23 @@
+require_relative 'main_application'
+require_relative 'item'
+require_relative 'cart'
+require_relative 'parser'
+
+app = MainApplication.new('admin@admin.com', 'adminadmin', 'cart_data')
+app.configure
+
+cart = Cart.new
+
+
+base_url = 'https://www.gutenberg.org/ebooks/'
+ids =[84, 46, 1342, 2701, 1513, 145, 2641, 37106, 100, 67979]
+urls = ids.map { |id| "#{base_url}#{id}" }
+
+urls.each do |url|
+  parsed_item = Parser.parse_item(url)
+  cart.add_item(parsed_item)
+  puts parsed_item.to_s
+end
+
+cart.save_to_json(app.file_path)
+cart.save_to_csv(app.file_path)
